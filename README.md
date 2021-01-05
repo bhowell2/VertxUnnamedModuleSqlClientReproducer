04c96c778fc6331c7c48983ffcaa490899256026
Blake <blakehowell2@gmail.com>
2021-01-05 10:15:55 -0800

    updated with more info and better main run.

--- README.md
+++ README.md
@@ -3,6 +3,108 @@ Trying to cast the rxified getConnection (or rxGetConnection) to PgConnection re
 
 `class io.vertx.reactivex.sqlclient.SqlConnection cannot be cast to class io.vertx.reactivex.pgclient.PgConnection (io.vertx.reactivex.sqlclient.SqlConnection and io.vertx.reactivex.pgclient.PgConnection are in unnamed module of loader 'app')`
 
+Non-rxified:
+```java
+// left out generics below
+
+abstract class PoolBase extends SqlClientBase implements Pool {
+
+  // PgPoolImpl will implement this, returning PgConnectionImpl (no problem non-rxified)
+  protected abstract SqlConnectionImpl wrap(ContextInternal context, Connection conn);
+
+  // relevant
+   @Override
+  public Future<SqlConnection> getConnection() {
+    ContextInternal current = vertx.getOrCreateContext();
+    Object metric;
+    if (metrics != null) {
+      metric = metrics.enqueueRequest();
+    } else {
+      metric = null;
+    }
+    Promise<Connection> promise = current.promise();
+    acquire(promise);
+    if (metrics != null) {
+      promise.future().onComplete(ar -> {
+        metrics.dequeueRequest(metric);
+      });
+    }
+    return promise.future().map(conn -> {
+      SqlConnectionImpl wrapper = wrap(current, conn);
+      conn.init(wrapper);
+      return wrapper;
+    });
+  }
+
+}
+
+
+class PgPoolImpl extends PoolBase implements PgPool {
+  // wrap is called by getConnection of PoolBase and gives you the PgConnection
+   @Override
+  protected SqlConnectionImpl wrap(ContextInternal context, Connection conn) {
+    return new PgConnectionImpl(factory, context, conn, tracer, metrics);
+  }
+}
+
+```
+
+RxVersion:
+```java
+
+@RxGen(io.vertx.sqlclient.Pool.class)
+public class Pool extends io.vertx.reactivex.sqlclient.SqlClient {
+  // this is where the error is. 
+  // PgPool below extends this class and passes PgPool as the delegate 
+  // delegate.getConnection() is called and returns PgConnectionImpl
+  // So far so good.
+  // HOWEVER, this is where it breaks down. now the returned PgConnectionImpl 
+  // is wrapped with the rxified SqlConnection rather than the rxified PgConnection
+  /**
+   * Get a connection from the pool.
+   * @param handler the handler that will get the connection result
+   */
+  public void getConnection(Handler<AsyncResult<io.vertx.reactivex.sqlclient.SqlConnection>> handler) { 
+    delegate.getConnection(new Handler<AsyncResult<io.vertx.sqlclient.SqlConnection>>() {
+      public void handle(AsyncResult<io.vertx.sqlclient.SqlConnection> ar) {
+        if (ar.succeeded()) {
+          handler.handle(io.vertx.core.Future.succeededFuture(io.vertx.reactivex.sqlclient.SqlConnection.newInstance((io.vertx.sqlclient.SqlConnection)ar.result())));
+        } else {
+          handler.handle(io.vertx.core.Future.failedFuture(ar.cause()));
+        }
+      }
+    });
+  }
+}
+
+@RxGen(io.vertx.pgclient.PgPool.class)
+public class PgPool extends io.vertx.reactivex.sqlclient.Pool {
+  
+  private final io.vertx.pgclient.PgPool delegate;
+  
+  public PgPool(io.vertx.pgclient.PgPool delegate) {
+    super(delegate);
+    this.delegate = delegate;
+  }
+
+}
+
+```
+
+To get around this issue, you call:
+```java
+  // rxified
+  io.vertx.reactivex.pgclient.PgPool.rxGetConnection(conn -> 
+    io.vertx.reactivex.pgclient.PgPool.rxGetConnection.newInstance(
+      (io.vertx.pgclient.PgConnection)conn.getDelegate()
+    )
+  )
+  // non-rxified version:
+  io.vertx.reactivex.pgclient.PgPool.rxGetConnection(conn -> 
+    (io.vertx.pgclient.PgConnection)conn.getDelegate()
+  )
+```
+
 ## Setup:
 Start postgres with password in the main class.
 `docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:13.1-alpine`
--- src/main/java/io/tst/MainNonVerticle.java
+++ src/main/java/io/tst/MainNonVerticle.java
@@ -6,50 +6,70 @@ import io.vertx.pgclient.impl.PgConnectionImpl;
 import io.vertx.reactivex.pgclient.PgPool;
 import io.vertx.reactivex.sqlclient.SqlConnection;
 import io.vertx.sqlclient.PoolOptions;
- 
+
 import java.lang.reflect.Constructor;
 
 
 public class MainNonVerticle {
 	public static void main(String[] args) {
 		PgConnectOptions connectOptions = new PgConnectOptions().setPort(5432).setHost("localhost").setPort(5432)
-				.setPassword("password").setDatabase("postgres").setUser("postgres");
+		                                                        .setPassword("password").setDatabase("postgres").setUser("postgres");
 		PoolOptions poolOptions = new PoolOptions().setMaxSize(5);
 		PgPool client = PgPool.pool(connectOptions, poolOptions);
-		// same thing happens with rxGetConnection. just using this one 
+		// same thing happens with rxGetConnection. just using this one
 		client.getConnection(connRes -> {
 			SqlConnection sqlConnection = connRes.result();
 			System.out.println("To string: " + sqlConnection);	// io.vertx.pgclient.impl.PgConnectionImpl
 			Constructor<?>[] constructors = sqlConnection.getClass().getConstructors();
 			System.out.println("Constructors: ");
 			for (int i = 0; i < constructors.length; i++) {
-			// 	io.vertx.reactivex.sqlclient.SqlConnection
-			// 	io.vertx.reactivex.sqlclient.SqlConnection
+				// 	io.vertx.reactivex.sqlclient.SqlConnection
+				// 	io.vertx.reactivex.sqlclient.SqlConnection
 				System.out.println(constructors[i].getName());
 			}
 			System.out.println("Simple name:" + sqlConnection.getClass().getSimpleName()); // SqlConnection
 			System.out.println("Name:" + sqlConnection.getClass().getName());	// io.vertx.reactivex.sqlclient.SqlConnection
 			System.out.println("Class: " + sqlConnection.getClass()); // io.vertx.reactivex.sqlclient.SqlConnection
 			System.out.println("Conn name: " + sqlConnection.getClass().getCanonicalName()); // io.vertx.reactivex.sqlclient.SqlConnection
-			try {
-				// io.vertx.reactivex.pgclient.PgConnection cast
-				PgConnection pgConn = (PgConnection) sqlConnection;
-			} catch (Throwable e) {
-				// fails
-				e.printStackTrace();
-			}
+			// the bottom 2 work:
 			try {
 				// just showing it doesnt extend this either
-			io.vertx.pgclient.PgConnection pgConnection = (io.vertx.pgclient.PgConnection) sqlConnection;
+				io.vertx.pgclient.PgConnection pgConnection = (io.vertx.pgclient.PgConnection) sqlConnection.getDelegate();
+				System.out.println("Worked.");
 			} catch (Throwable e) {
 				// fails
 				e.printStackTrace();
 			}
 			try {
-				PgConnectionImpl pgConn = (PgConnectionImpl) (Object)sqlConnection;
-			} catch (Exception e) {
+				io.vertx.reactivex.pgclient.PgConnection pgConnection2 = io.vertx.reactivex.pgclient.PgConnection.newInstance((io.vertx.pgclient.PgConnection) sqlConnection.getDelegate());
+				System.out.println("Rxified. Worked.");
+			} catch (Throwable e) {
 				e.printStackTrace();
 			}
+
+			// fails
+			try {
+				io.vertx.reactivex.pgclient.PgConnection pgConn = (io.vertx.reactivex.pgclient.PgConnection) sqlConnection;
+			} catch (Throwable e) {
+				// fails
+				System.out.println(e.getMessage());
+//				e.printStackTrace();
+			}
+			// fails
+			try {
+				io.vertx.pgclient.PgConnection pgConn = (io.vertx.pgclient.PgConnection) sqlConnection;
+			} catch (Throwable e) {
+				System.out.println(e.getMessage());
+//				e.printStackTrace();
+			}
+			// fails
+			try {
+				io.vertx.reactivex.pgclient.PgConnection pgConn = (io.vertx.reactivex.pgclient.PgConnection) sqlConnection.getDelegate();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+//				e.printStackTrace();
+			}
+
 			client.close();
 		});
 	}
